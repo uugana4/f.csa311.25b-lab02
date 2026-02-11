13c610f34cc8003c0b2065219f2cce1f24d1b1a9
uugana4 <uuganbayr126@icloud.com>
2026-02-11 08:34:40 +0800

    Your commit message

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -2,11 +2,15 @@ package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 
 
 public class Main {
     public static void main(String[] args) {
-        Shape shape = new Circle(2);
+        Shape shape = new Circle(5);
+        shape = new Rectangle(4, 6);
+        shape = new Square(3);
         Renderer renderer = new Renderer(shape);
 
         renderer.draw();
